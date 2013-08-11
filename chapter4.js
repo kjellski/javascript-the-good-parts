465308867da3d645fa647d1316d5adc4b3880b67
@kjellski <otto.kjell@gmail.com>
2013-08-11 14:30:01 +0200

    added the "Function Invocation Pattern"

--- chapter4.js
+++ chapter4.js
@@ -19,3 +19,32 @@ methodInvocationPattern.increment();
 console.log("value: " + methodInvocationPattern.value);
 methodInvocationPattern.increment(2);
 console.log("value: " + methodInvocationPattern.value);
+
+// #######################################################################################
+// The function invcation pattern is the one that is somewhat bad designed.
+// the inner function should get its this bound to the enclosing function,
+// but instead, it's bound to the global scope.
+var functionInvocationPattern = {
+	value: 0,
+	sum: function () {
+		// this "this" is what we expect from the "Method Invocation Pattern"
+		console.log("inside sums this: ", this); 
+		var insideSumFunction = function (argsOfSum) {
+			// this "this" is the design error, it's not bound to the outer
+			// closure, it's bound to the global object: "[object global]"
+			console.log("inside innersums this: " + this);
+			var result = 0;
+			for (var i = argsOfSum.length - 1; i >= 0; i--) {
+				result += argsOfSum[i];
+			};
+			return result;
+		};
+
+		this.value = insideSumFunction(arguments);
+	}
+};
+
+console.log("functionInvocationPattern");
+console.log("value: " + functionInvocationPattern.value);
+functionInvocationPattern.sum(1,2,3,4,5);
+console.log("value: " + functionInvocationPattern.value);
